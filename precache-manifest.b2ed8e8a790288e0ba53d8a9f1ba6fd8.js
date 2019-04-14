7af589e5fe21618c606719ecf2f57262f6f01bf2
EvgeniyDz <zhekan1502@gmail.com>
2019-04-14 17:44:26 +0300

    Updates

---
+++ precache-manifest.b2ed8e8a790288e0ba53d8a9f1ba6fd8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/shop/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "03f641275216e9d13b15",
+    "url": "/shop/static/css/main.b05b6a6e.chunk.css"
+  },
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  },
+  {
+    "revision": "ee27ecd383a262cfb371",
+    "url": "/shop/static/js/2.1efdb40e.chunk.js"
+  },
+  {
+    "revision": "cefea94cf3d49d9f37b9b6f2332771c8",
+    "url": "/shop/static/media/hero-banner.cefea94c.png"
+  },
+  {
+    "revision": "03f641275216e9d13b15",
+    "url": "/shop/static/js/main.e46d06a8.chunk.js"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/shop/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/shop/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/shop/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "47da820d415af1a242c7ad9c05e086ed",
+    "url": "/shop/static/media/parallax-bg.47da820d.png"
+  },
+  {
+    "revision": "b3e56bb605088643ac58ac822a0e722d",
+    "url": "/shop/index.html"
+  }
+];
